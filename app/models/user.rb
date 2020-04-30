d96218e22a58e45f5b5703d10edbeae3a9a8cf8e
ryohei udagawa <r.udagawa1995@gmail.com>
2020-04-30 09:02:10 +0900

    seed.rb検証

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,15 @@
 class User < ApplicationRecord
   mount_uploader :face, FaceUploader
-  validates :name ,presence: true
-  validates :email, presence: true ,uniqueness: true ,format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }  
-  validates :face, presence: true
-  before_validation { email.downcase! }
+  
   has_secure_password
+  before_validation { email.downcase! }
+
+  validates :name ,presence: true
+  validates :email, presence: true ,uniqueness: true ,format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+  validates :face, presence: true
+
   validates :password, presence: true, length: {minimum: 6 }
+
   has_many :pictures
   has_many :favorites, dependent: :destroy
   has_many :favorite_pictures, through: :favorites, source: :picture
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,24 +1,22 @@
   50.times do |n|
   name = Faker::Games::Pokemon.name
   email = Faker::Internet.email
-  face = Faker::Avatar.image
   password = "password"
   User.create!(name: name,
                email: email,
-               face: face,
+               face: open("./public/uploads/user/404.png"),
                password: password,
                password_confirmation: password,
                )
 end
 
 User.all.each do |user|
-  title = Faker::Games::Pokemon.title
-  content = Faker::Name.content
-  image = Faker::Avatar.image
+  title = Faker::Games::Pokemon.name
+  content = Faker::Games::Pokemon.name
   user.pictures.create!(
     title: title,
     content: content,
-    image: image,
+    image: open("./public/uploads/picture/500.png"),
     user_id: user.id
   )
 end
