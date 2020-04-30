10f2a184839aac4a4fe36005e47ffadf2c703031
ryohei udagawa <r.udagawa1995@gmail.com>
2020-04-30 11:31:18 +0900

    userの削除修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :edit, :update, :destroy]
+  before_action :set_user, only: [:show, :edit, :update]
 
   def index
     @users = User.all
@@ -42,14 +42,6 @@ class UsersController < ApplicationController
         render :edit
         render json: @user.errors, status: :unprocessable_entity
       end
-    end
-  end
-
-  def destroy
-    @user.destroy
-    respond_to do |format|
-      format.html { redirect_to users_url, notice: 'User was successfully destroyed.' }
-      format.json { head :no_content }
     end
   end
 
